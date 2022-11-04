ba4915fd40250f2a3fcf6506c8954b04ac5786e2
João Pedro Wolschick <joaopedrowols@gmail.com>
2022-11-04 14:05:06 -0300

    Refatoração básica

---
+++ lib/features/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'home/my_home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:genshin_project/main.dart';
+import 'package:genshin_project/features/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
